55ae53aacad0073d52379af318e317181bf48de1
Alessandro <alessandrocatalano999@gmail.com>
2021-06-14 23:10:56 +0200

    Moved menu stuff to dedicated package

---
+++ Source/GtaVModPeDistance/Menu/Menu.cs
@@ -0,0 +1,59 @@
+﻿using NativeUI;
+using System.Collections.Generic;
+
+namespace GtaVModPeDistance
+{
+    public class Menu
+    {
+        private List<MenuItem> itemList;
+        private UIMenu _modMenu;
+        public UIMenu ModMenu
+        {
+            get => _modMenu;
+            set => _modMenu = value;
+        }
+
+        public Menu(string menuName, string menuSubtitle, List<MenuItem> itemList)
+        {
+            _modMenu = new UIMenu(menuName, menuSubtitle);
+            SetupMenu(_modMenu, itemList);
+        }
+
+        public Menu(UIMenu modMenu, List<MenuItem> itemList)
+        {
+            _modMenu = modMenu;
+            SetupMenu(_modMenu, itemList);
+        }
+
+        private void SetupMenu(UIMenu modMenu, List<MenuItem> itemList)
+        {
+            _modMenu.OnItemSelect += onMainMenuItemSelected;
+            this.itemList = itemList;
+            if (itemList.Count > 0)
+                itemList.ForEach(x => modMenu.AddItem(x.Item));
+        }
+
+        void onMainMenuItemSelected(UIMenu sender, UIMenuItem item, int index)
+        {
+            itemList.ForEach(x => x.Evaluate(item));
+        }
+
+        public void ToggleMenu()
+        {
+            _modMenu.Visible = !_modMenu.Visible;
+        }
+
+        public void ShowMenu()
+        {
+            _modMenu.Visible = true;
+        }
+
+        public void HideMenu()
+        {
+            _modMenu.Visible = false;
+        }
+
+    }
+
+    
+}
